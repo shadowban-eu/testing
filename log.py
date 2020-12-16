af360ffca143630ac322c3ecfd67eec9247eaaa6
Raphael Beer <raphael.beer@protonmail.com>
2020-12-16 02:50:02 +0100

    Add: gzip log files on rotation

--- log.py
+++ log.py
@@ -1,14 +1,28 @@
+import gzip
 import logging
 from logging.handlers import TimedRotatingFileHandler
+import os
+import shutil
 
 log_format = '%(asctime)s | %(module)s:%(lineno)d | %(levelname)s: %(message)s'
 logging.basicConfig(format=log_format)
 
 log = logging.getLogger(__name__)
 
+def file_namer(name):
+    return name + ".gz"
+
+def file_rotator(source, dest):
+    with open(source, "rb") as sf:
+        with gzip.open(dest, "wb") as df:
+            shutil.copyfileobj(sf, df)
+    os.remove(source)
+
 def add_file_handler(filename):
     handler = TimedRotatingFileHandler(filename=filename, when='midnight')
     handler.setFormatter(logging.Formatter(fmt=log_format))
+    handler.namer = file_namer
+    handler.rotator = file_rotator
     log.addHandler(handler)
     log.info('Writing log to %s', filename)
 
